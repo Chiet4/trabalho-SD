5e4218bc8bb1957c49e3ba7c18276089fe252154
Anchieta N. Albano <anchietaalbano@alu.ufc.br>
2024-09-07 17:03:13 -0300

    Update Despachante e Add Lado_ClienteUDP

---
+++ Lado_ClienteUDP/Proxy.py
@@ -0,0 +1,122 @@
+from UDPClient import UDPClient
+import json
+import logging
+
+# Configuração do logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class Proxy:
+    def __init__(self, hostname, port):
+        self.request_id = 0
+        self.client = UDPClient(hostname, port)
+
+    def reservar_ticket(self, cpf, data, hora, origem, destino, nome, poltrona):
+        request = self.doOperation("reservar_ticket", {
+            "cpf": cpf,
+            "data": data,
+            "hora": hora,
+            "origem": origem,
+            "destino": destino,
+            "nome": nome,
+            "poltrona": poltrona
+        })
+        self.client.send_request(request)
+        response = self.client.receive_response()
+        if response:
+            response = self.doOperation(response)
+            logger.info(f"Response for reservar_ticket: {response}")
+            print(f"Resposta: {response}")
+            return response
+        else:
+            logger.error("No response received from server")
+            return None
+
+    def atualizar_reserva(self, ticket_id, cpf, data, hora, origem, destino, nome, poltrona):
+        request = self.doOperation("atualizar_reserva", {
+            "ticket_id": ticket_id,
+            "cpf": cpf,
+            "data": data,
+            "hora": hora,
+            "origem": origem,
+            "destino": destino,
+            "nome": nome,
+            "poltrona": poltrona
+        })
+        self.client.send_request(request)
+        response = self.client.receive_response()
+        if response:
+            response = self.doOperation(response)
+            print(f"Resposta: {response}")
+            return response
+        else:
+            logger.error("No response received from server")
+            return None
+
+    def cancelar_reserva(self, ticket_id):
+        request = self.doOperation("cancelar_reserva", {
+            "ticket_id": ticket_id
+        })
+        self.client.send_request(request)
+        response = self.client.receive_response()
+        if response:
+            response = self.doOperation(response)
+            print(f"Resposta: {response}")
+            return response
+        else:
+            logger.error("No response received from server")
+            return None
+
+    def consultar_reserva(self, cpf):
+        request = self.doOperation("consultar_reserva", {
+            "cpf": cpf
+        })
+        self.client.send_request(request)
+        response = self.client.receive_response()
+        if response:
+            response = self.doOperation(response)
+            print(f"Resposta: {response}")
+            return response
+        else:
+            logger.error("No response received from server")
+            return None
+
+    def doOperation(self, messageType, params=None):
+        # Incrementando o request_id para cada operação
+        self.request_id += 1
+
+        if params is not None:
+            # Criação da mensagem de requisição
+            message = {
+                "messageType": 0,  # 0 = Request
+                "requestId": self.request_id,
+                "methodId": messageType,
+                "arguments": params
+            }
+            # Serialização da mensagem em JSON
+            return json.dumps(message)
+        else:
+            # Processamento da resposta
+            message = json.loads(messageType)  # Desserializa a resposta JSON
+
+            if message["messageType"] == 1:  # 1 = Reply
+                arguments = message["arguments"]
+
+                if isinstance(arguments, str):
+                    try:
+                        arguments_json = json.loads(arguments)
+                    except json.JSONDecodeError as e:
+                        logger.error(f"Failed to decode JSON: {e}")
+                        return None
+                else:
+                    arguments_json = arguments
+                # Verificar se houve sucesso na operação
+                response = {
+                    "result": arguments_json,
+                    "status": message["status"]
+                }
+                return response
+
+    def close(self):
+        self.client.close()
+
---
+++ Lado_ClienteUDP/UDPClient.py
@@ -0,0 +1,40 @@
+import socket
+import json
+import logging
+
+# Configuração do logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class UDPClient:
+    def __init__(self, hostname, port):
+        self.server_address = (hostname, port)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        logger.info(f"UDPClient initialized with server address: {self.server_address}")
+
+    def send_request(self, request):
+        try:
+            # Serializa a mensagem inteira como JSON e então converte para bytes
+
+            self.socket.sendto(request.encode('utf-8'), self.server_address)
+            logger.info(f"Sent request: {request}")
+        except Exception as e:
+            logger.error(f"Failed to send request: {e}")
+
+    def receive_response(self):
+        try:
+            # Recebe a resposta como um pacote de bytes
+            packet, _ = self.socket.recvfrom(4096)  # Buffer de 4096 bytes
+            json_response = packet.decode('utf-8')  # Decodifica de volta para string
+            logger.info(f"Received response: {json_response}")
+            return json_response  # Retorna como JSON
+        except json.JSONDecodeError as e:
+            logger.error(f"Failed to decode JSON: {e}")
+            return None
+        except Exception as e:
+            logger.error(f"Failed to receive response: {e}")
+            return None
+
+    def close(self):
+        self.socket.close()
+        logger.info("Socket closed")
